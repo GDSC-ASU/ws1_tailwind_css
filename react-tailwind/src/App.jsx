5651e29a8d8c9655c2da42fa1c39540101b3fd43
Mohammad Khasawneh <mohdkh2010@live.com>
2023-03-10 22:45:02 +0300

    added blog title prop

--- react-tailwind/src/App.jsx
+++ react-tailwind/src/App.jsx
@@ -6,12 +6,15 @@ function App() {
     <Navbar />
     <div className="px-4 mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
       <Blog imgLink="https://149841302.v2.pressablecdn.com/wp-content/uploads/2022/02/tailwindcss-templates.png"
+        blogTitle="Tailwind"
         blogDesc="Tailwind CSS is an open source CSS framework. The main feature of this library is that,
       unlike other CSS frameworks, it does not provide a series of predefined classes for elements."/>
       <Blog imgLink="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwYRPiy-a70SBKEJaFAAFPORNd174AJMvVzA&usqp=CAU"
+        blogTitle="Visual Studio Code"
         blogDesc="Visual Studio Code, also commonly referred to as VS Code,
       is a source-code editor made by Microsoft with the Electron Framework, for Windows, Linux and macOS."/>
       <Blog imgLink="https://www.nixsolutions.com/uploads/2020/07/Golang-700x395.png"
+        blogTitle="Go Programming Language"
         blogDesc="Go is a statically typed, compiled high-level programming language designed at Google by Robert
       Griesemer,
       Rob Pike, and Ken Thompson"/>
--- react-tailwind/src/components/Blog.jsx
+++ react-tailwind/src/components/Blog.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-function Blog({ imgLink, blogDesc }) {
+function Blog({ imgLink, blogDesc, blogTitle}) {
     return <div>
         <div className="bg-white hover:shadow-lg">
             <img className="h-48 w-full"
                 src={imgLink}
                 alt="Blog Post" />
             <div className="p-4">
-                <h2 className="text-xl font-bold text-gray-800">Tailwind CSS</h2>
+                <h2 className="text-xl font-bold text-gray-800">{blogTitle}</h2>
                 <p className="mt-2 text-gray-600">
                     {blogDesc}
                 </p>
